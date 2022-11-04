45157da20a14b2230bcd3d9841f60dfcdcc83304
bakisultanov@gmail.com <bakisultanov@gmail.com>
2022-11-04 12:49:37 +0300

    dop_2

---
+++ main.js
@@ -0,0 +1,16 @@
+let a = prompt('Enter the color');
+
+switch(a) {
+    case 'red':
+        alert('Stop');
+        break;
+    case 'yellow':
+        alert('Wait');
+        break;
+    case 'green':
+        alert('go!');
+        break;
+    default:
+        alert('Enter the Traffic light color');
+        break;
+}
